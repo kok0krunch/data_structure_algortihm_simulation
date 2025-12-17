0069dccbd1b5feff44475fda2ffa352021dc7af6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-17 09:04:11 +0800

    Added the program that creates the loop to ask user's input continually until conditions are met.

--- core_programs/4_binary_search_tree.py
+++ core_programs/4_binary_search_tree.py
@@ -1,7 +1,16 @@
+inputs=0
+
 #Binary Search Tree
 print("BINARY SEARCH TREE: Input a number and we will make a binary search tree for you.")
 first_num=int(input("Input your number:"))#Ask user to input number
 print (first_num)#First number is directly outputted
-#Suceeding numbers will be identified whether greater or smaller than the first number and preceding numbers.
+inputs+=1
+print(f"inputs:{inputs}") #REMOVE THIS IN FINAL (FOR CHECKING ONLY)
+while True: #Program will ask for user input until user types 'Done' or they have reached the maximum inputs (100 inputs)
+    try:
+        while inputs!=100:
+            ...
+    except ValueError:
+        print("You have typed in 'DONE'. Creating your tree...")
+#Suceeding numbers will be identified whether greater or smaller than the first number and preceeding numbers.
 #Smaller numbers go the left, larger numbers go to the right
-#Program will ask for user input until user types 'Done' or they have reached the maximum inputs (100 inputs)
