69898441f0c3ca4584320fcbcdc64e1e3d50276b
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-17 08:44:57 +0800

    Pseudocode (Binary search tree)

---
+++ core_programs/4_binary_search_tree.py
@@ -0,0 +1,6 @@
+#Binary Search Tree
+#Ask user to input number
+#First number is directly outputted
+#Suceeding numbers will be identified whether greater or smaller than the first number and preceding numbers.
+#Smaller numbers go the left, larger numbers go to the right
+#Program will ask for user input until user types 'Done' or they have reached the maximum inputs (100 inputs)
