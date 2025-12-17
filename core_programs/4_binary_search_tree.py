ee11c143ee2a935c990ab31856a813875bca541c
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-17 09:46:01 +0800

    deleted everything.  Ihave to plan through first, because it gets confusing

--- core_programs/4_binary_search_tree.py
+++ core_programs/4_binary_search_tree.py
@@ -1,16 +1,8 @@
-inputs=0
-
 #Binary Search Tree
-print("BINARY SEARCH TREE: Input a number and we will make a binary search tree for you.")
-first_num=int(input("Input your number:"))#Ask user to input number
-print (first_num)#First number is directly outputted
-inputs+=1
-print(f"inputs:{inputs}") #REMOVE THIS IN FINAL (FOR CHECKING ONLY)
-while True: #Program will ask for user input until user types 'Done' or they have reached the maximum inputs (100 inputs)
-    try:
-        while inputs!=100:
-            ...
-    except ValueError:
-        print("You have typed in 'DONE'. Creating your tree...")
-#Suceeding numbers will be identified whether greater or smaller than the first number and preceeding numbers.
+#Ask user to input number
+#First number is directly outputted
+#REMOVE THIS IN FINAL (FOR CHECKING ONLY)
+#REMOVE THIS IN FINAL (FOR CHECKING ONLY)
+#Program will ask for user input until user types 'Done' or they have reached the maximum inputs (100 inputs)
+#Suceeding numbers will be identified whether greater or smaller than the first number and preceding numbers.
 #Smaller numbers go the left, larger numbers go to the right
