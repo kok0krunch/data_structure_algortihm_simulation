1ca5a68969ae29b1e43418d67be73b32d11bb693
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-18 20:56:04 +0800

    checked if the root becomes the first number typed. Gave error, asked AI to help me make it work and was adviced to separate node attributes to the main tree attributes. It's now working, teehe

--- core_programs/4_binary_search_tree.py
+++ core_programs/4_binary_search_tree.py
@@ -1,15 +1,19 @@
 # Binary Search Tree
-class BinarySearchTree: # Use class
-    def __init__(self, number, left, right):
+# Use class
+class Node:
+    def __init__(self,number,left,right): # Class attributes: top, left, right
         self.number=number
         self.left=left
-        self.right=right # Class attributes: top, left, right
+        self.right=right
+
+class BinarySearchTree: 
+    def __init__(self):
         self.root=None
-    
+
     # Functions needed
-    def insert(self):
+    def insert(self, number):
         if self.root==None: # First number as root
-            self.root=self.number
+            self.root=number
             return self.root
         
             # Determine if number is bigger or smaller
@@ -21,11 +25,15 @@ class BinarySearchTree: # Use class
     #Use definition
         # input
 def input_number():
-    number_input=int(input("Enter number:"))
-    BinarySearchTree.number=number_input
+    number_input=input("Enter number:")
+    binary_search_tree.number=number_input
+    print(binary_search_tree.number)
+    binary_search_tree.insert(number_input)
+    print(binary_search_tree.root)
     #Ask user to input number (input)
     # main
 
+binary_search_tree=BinarySearchTree()
 input_number()
             # Enable user to input until maximum input is reached(31 inputs)(Use while?)
             # If number is reached/ typed done. Print tree
