34a6a394e66c42d68a7226767b1ab61e8cb7a38a
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-22 18:27:18 +0800

    fixed the branching code, put back the try and except, and added code for printing the tree(taken from a source, not sure if I will keep it, but it is currently only there so I can see if my binary search tree values is correct.

--- core_programs/4_binary_search_tree.py
+++ core_programs/4_binary_search_tree.py
@@ -18,21 +18,38 @@ class BinarySearchTree:
             self.branching(self.root,number)
 
     def branching(self,current,number):
-        if number>current.number: # smaller/equal = left
-            if self.left is None:
+        if number<=current.number: # smaller/equal = left
+            if current.left is None:
                 current.left=Node(number) # Traverse the tree until it reaches an empty node
             else:
-                self.branching(number)  # Insert in binary tree
+                self.branching(current.left,number)  # Insert in binary tree
         else: # bigger = right
-            if self.right is None:
+            if current.right is None:
                 current.right=Node(number) # Traverse the tree until it reaches an empty node
             else:
-                self.branching(number) # Insert in binary tree
+                self.branching(current.right,number) # Insert in binary tree
 
+        # 🌳 PRINT TREE
+    def print_tree(self):
+        self._print_tree(self.root, 0)
+
+    def _print_tree(self, node, level):
+        if node is not None:
+            self._print_tree(node.right, level + 1)
+            print("   " * level + str(node.number))
+            self._print_tree(node.left, level + 1)
 # main
 inputted_number=0
 binary_search_tree=BinarySearchTree()
 while inputted_number!=100:# Enable user to input until maximum input is reached(31 inputs)(Use while?)
-    user_input=int(input("Enter number:"))
-    binary_search_tree.insert(user_input)
-# If number is reached/ user typed done. Print tree
+    try:
+        user_input=int(input("Enter number:"))
+        binary_search_tree.insert(user_input)
+
+    except:
+        print("You have entered a non-integer/have typed done. Creating tree") # If number is reached/ user typed done. Print tree
+        binary_search_tree.print_tree()
+        break
+
+print("You have reached the maximum amount of inputs. Creating tree.") # If number is reached/ user typed done. Print tree
+binary_search_tree.print_tree()
