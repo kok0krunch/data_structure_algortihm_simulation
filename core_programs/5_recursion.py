a8db5595d3e107828abaa0bd39444a0ad568d9cb
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-12-27 05:45:46 +0800

    the code is working. however, it makes unnecessary moves or makes too much moves than the desired moves as listed in the terminal

--- core_programs/5_recursion.py
+++ core_programs/5_recursion.py
@@ -10,13 +10,14 @@ print(f"It takes {num_moves} to solve for the tower")
 def TowerOfHanoi(user_input, tower_1, tower_2, tower_3):
     if user_input == 0:
         return
-    elif user_input == 1:
-        print("Move disk from", tower_1, "to", tower_3)
-        return
     
-    TowerOfHanoi(user_input - 1, tower_1, tower_2, tower_3)
+    TowerOfHanoi(int(user_input) - 1, tower_1, tower_2, tower_3)
     print('Move disk', user_input, 'from', tower_1, 'to', tower_3)
-    TowerOfHanoi(user_input - 1, tower_2, tower_3, tower_1)
+    TowerOfHanoi(int(user_input) - 1, tower_1, tower_2, tower_3)
     print('Move disk', user_input, 'from', tower_1, 'to', tower_2)
 
-TowerOfHanoi(user_input, tower_1, tower_2, tower_3) 
+TowerOfHanoi(user_input, 'tower_1', 'tower_2', 'tower_3') 
+
+
+
+#note: possible na i ask pano sya ilagay sa tower 2 or maybe yung placement ng disks ay nasa tower 3
