b13ac4b29b2b21121a9fb627e54ab21a09f4491d
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-12-27 06:12:37 +0800

    fixed the logic and made changes to the function that calculates the number of movement it takes to complete the puzzle

--- core_programs/5_recursion.py
+++ core_programs/5_recursion.py
@@ -1,20 +1,21 @@
 # Tower of Hanoi
 
 # Ask user how many disks
-user_input = input("How many disks would you like? ")
+user_input = int(input("How many disks would you like? "))
 
 # Calculate how many moves it takes to solve the puzzle
-num_moves = int(user_input) ** 2 - 1
+num_moves = 2 ** int(user_input) - 1
 print(f"It takes {num_moves} to solve for the tower")
 
 def TowerOfHanoi(user_input, tower_1, tower_2, tower_3):
     if user_input == 0:
         return
     
-    TowerOfHanoi(int(user_input) - 1, tower_1, tower_2, tower_3)
+    TowerOfHanoi(int(user_input) - 1, tower_1, tower_3, tower_2)
     print('Move disk', user_input, 'from', tower_1, 'to', tower_3)
-    TowerOfHanoi(int(user_input) - 1, tower_1, tower_2, tower_3)
-    print('Move disk', user_input, 'from', tower_1, 'to', tower_2)
+    TowerOfHanoi(int(user_input) - 1, tower_2, tower_1, tower_3)
+    
+    
 
 TowerOfHanoi(user_input, 'tower_1', 'tower_2', 'tower_3') 
 
