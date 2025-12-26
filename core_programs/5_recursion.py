b0a03c78736820dc122ed786cbd99b054df53485
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-12-27 04:33:03 +0800

    adding a user input so that the user can input how many disks they want and also added how many moves will it take to solve the tower

--- core_programs/5_recursion.py
+++ core_programs/5_recursion.py
@@ -1,1 +1,8 @@
 # Tower of Hanoi
+
+# Ask user how many disks
+user_input = input("How many disks would you like? ")
+
+# Calculate how many moves it takes to solve the puzzle
+num_moves = int(user_input) ** 2 - 1
+print(f' It takes {num_moves} to solve for the tower')
