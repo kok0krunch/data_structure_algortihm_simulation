5c63601fd0ccd433dd22071114beedc9c9f4fe9b
justinroyse <justinroyselsolomon@gmail.com>
2025-12-26 14:37:09 +0800

    Updated: Pseudocode

--- core_programs/2_queue.py
+++ core_programs/2_queue.py
@@ -1,6 +1,7 @@
 #Queue Data Structure Core Program
 #Queue Logic Python File
 #Implement Enqqueue(), Dequeue(), Peek(), isEmpty(), and size operations as Python Functions
+#Implement Table for license plate, number of arrival, number of departure, and who arrived and departed
 
 class Queue:
     def __init__(self):
