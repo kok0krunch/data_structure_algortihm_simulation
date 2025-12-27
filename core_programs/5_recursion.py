a84416578ae6bf9af74b6c3a8abf31a3839dabe6
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-12-27 16:53:35 +0800

    feat: adding user inputs for origin and destination of rings and and logic for transferring rings: from tower 1 to 2, tower 2 to 1

--- core_programs/5_recursion.py
+++ core_programs/5_recursion.py
@@ -2,23 +2,40 @@
 
 # Ask user how many disks
 user_input = int(input("How many disks would you like? "))
-
+choose_origin_tower = int(input("From which tower do you wish to start the puzzle?:(1, 2, 3) "))
+choose_destination_tower = int(input("Where do you eish to put the disks?:(1, 2, 3) "))
+if choose_origin_tower == choose_destination_tower:
+    print('Invalid input')
 # Calculate how many moves it takes to solve the puzzle
 num_moves = 2 ** int(user_input) - 1
 print(f"It takes {num_moves} to solve for the tower")
 
-def TowerOfHanoi(user_input, tower_1, tower_2, tower_3):
+def TowerOneToTowerThree(user_input, tower_1, tower_2, tower_3):
     if user_input == 0:
         return
     
-    TowerOfHanoi(int(user_input) - 1, tower_1, tower_3, tower_2)
+    TowerOneToTowerThree(int(user_input) - 1, tower_1, tower_3, tower_2)
     print('Move disk', user_input, 'from', tower_1, 'to', tower_3)
-    TowerOfHanoi(int(user_input) - 1, tower_2, tower_1, tower_3)
+    TowerOneToTowerThree(int(user_input) - 1, tower_2, tower_1, tower_3)
+
+def TowerOneToTowerTwo(user_input, tower_1, tower_2, tower_3):
+    if user_input == 0:
+        return
     
+    TowerOneToTowerTwo(int(user_input) - 1, tower_1, tower_3, tower_2)
+    print("Move disk", user_input, "from", tower_1, "to", tower_2)
+    TowerOneToTowerTwo(int(user_input) - 1, tower_3, tower_2, tower_1)
+
+def TowerTwoToTowerOne(user_input, tower_1, tower_2, tower_3):
+    if user_input:
+        return
     
+    TowerTwoToTowerOne(int(user_input) - 1, tower_2, tower_1, tower_3)
+    print("Move disk", user_input, "from", tower_2, "to", tower_1)
+    TowerTwoToTowerOne(int(user_input) - 1, tower_3, tower_2, tower_1)
 
-TowerOfHanoi(user_input, 'tower_1', 'tower_2', 'tower_3') 
-
-
+TowerTwoToTowerOne(user_input, 'tower_1', 'tower_2', 'tower_3')
+TowerOneToTowerTwo(user_input, 'tower_1', 'tower_2', 'tower_3')
+TowerOneToTowerThree(user_input, 'tower_1', 'tower_2', 'tower_3') 
 
 #note: possible na i ask pano sya ilagay sa tower 2 or maybe yung placement ng disks ay nasa tower 3
