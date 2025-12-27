975d0db4c743020e097c6153d36e9f18995fef0b
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-12-28 01:58:40 +0800

    style:added a main menu that will execute what the user wants to do

--- core_programs/5_recursion.py
+++ core_programs/5_recursion.py
@@ -11,56 +11,70 @@ num_moves = 2 ** int(user_input) - 1
 print(f"It takes {num_moves} to solve for the tower")
 
 # Functions for solving Tower of Hanoi using recursion
-def TowerOneToTowerTwo(user_input, tower_1, tower_2, tower_3):
+def tower_one_to_tower_two(user_input, tower_1, tower_2, tower_3):
     if user_input == 0:
         return
-    TowerOneToTowerTwo(int(user_input) - 1, tower_1, tower_3, tower_2)
+    tower_one_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
     print("Move disk", user_input, "from", tower_1, "to", tower_2)
-    TowerOneToTowerTwo(int(user_input) - 1, tower_3, tower_2, tower_1)
+    tower_one_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
 
-def TowerOneToTowerThree(user_input, tower_1, tower_2, tower_3):
+def tower_one_to_tower_three(user_input, tower_1, tower_2, tower_3):
     if user_input == 0:
         return
     
-    TowerOneToTowerThree(int(user_input) - 1, tower_1, tower_3, tower_2)
+    tower_one_to_tower_three(int(user_input) - 1, tower_1, tower_3, tower_2)
     print('Move disk', user_input, 'from', tower_1, 'to', tower_3)
-    TowerOneToTowerThree(int(user_input) - 1, tower_2, tower_1, tower_3)
+    tower_one_to_tower_three(int(user_input) - 1, tower_2, tower_1, tower_3)
 
-def TowerTwoToTowerOne(user_input, tower_2, tower_3, tower_1):
+def tower_two_to_tower_one(user_input, tower_2, tower_3, tower_1):
     if user_input == 0:
         return
     
-    TowerTwoToTowerOne(int(user_input) - 1, tower_2, tower_1, tower_3)
+    tower_two_to_tower_one(int(user_input) - 1, tower_2, tower_1, tower_3)
     print("Move disk", user_input, "from", tower_2, "to", tower_1)
-    TowerTwoToTowerOne(int(user_input) - 1, tower_3, tower_2, tower_1)
+    tower_two_to_tower_one(int(user_input) - 1, tower_3, tower_2, tower_1)
 
-TowerTwoToTowerOne(user_input, 'tower_2', 'tower_3', 'tower_1')
-
-def TowerTwoToTowerThree(user_input, tower_2, tower_1, tower_3):
+def tower_two_to_tower_three(user_input, tower_2, tower_1, tower_3):
     if user_input == 0:
         return
-    TowerTwoToTowerThree(int(user_input) - 1, tower_2, tower_3, tower_1)
+    tower_two_to_tower_three(int(user_input) - 1, tower_2, tower_3, tower_1)
     print("Move disk", user_input, "from", tower_2, "to", tower_3)
-    TowerTwoToTowerThree(int(user_input) - 1, tower_1, tower_2, tower_3)
+    tower_two_to_tower_three(int(user_input) - 1, tower_1, tower_2, tower_3)
 
-def TowerThreeToTowerOne(user_input, tower_3, tower_2, tower_1):
+def tower_three_to_tower_one(user_input, tower_3, tower_2, tower_1):
     if user_input == 0:
         return
-    TowerThreeToTowerOne(int(user_input) - 1, tower_3, tower_1, tower_2)
+    tower_three_to_tower_one(int(user_input) - 1, tower_3, tower_1, tower_2)
     print("Move disk", user_input, "from", tower_3, "to", tower_1)
-    TowerThreeToTowerOne(int(user_input) - 1, tower_2, tower_3, tower_1)
+    tower_three_to_tower_one(int(user_input) - 1, tower_2, tower_3, tower_1)
 
-def TowerThreeToTowerTwo(user_input, tower_3, tower_1, tower_2):
+def tower_three_to_tower_two(user_input, tower_3, tower_1, tower_2):
     if user_input == 0:
         return
-    TowerThreeToTowerTwo(int(user_input) - 1, tower_3, tower_2, tower_1)
+    tower_three_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
     print("Move disk", user_input, "from", tower_3, "to", tower_2)
-    TowerThreeToTowerTwo(int(user_input) - 1, tower_1, tower_3, tower_2)
+    tower_three_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
+
+def main_menu():
+    if choose_origin_tower == 1 and choose_destination_tower == 2:
+        tower_one_to_tower_two(user_input, 'tower_1', 'tower_2', 'tower_3')
+    elif choose_origin_tower == 1 and choose_destination_tower == 3:
+        tower_one_to_tower_three(user_input, 'tower_1', 'tower_2', 'tower_3') 
+    elif choose_origin_tower == 2 and choose_destination_tower == 1:
+        tower_two_to_tower_one(user_input, 'tower_2', 'tower_3', 'tower_1')
+    elif choose_origin_tower == 2 and choose_destination_tower == 3:
+        tower_two_to_tower_three(user_input, 'tower_2', 'tower_1', 'tower_3')
+    elif choose_origin_tower == 3 and choose_destination_tower == 2:
+        tower_three_to_tower_two(user_input, 'tower_3', 'tower_1', 'tower_2')
+    elif choose_origin_tower == 3 and choose_destination_tower == 1:
+        tower_three_to_tower_one(user_input, 'tower_3', 'tower_2', 'tower_1')
+
+
+
+
+
+ 
+    
+
 
 # Calls the functions
-TowerOneToTowerTwo(user_input, 'tower_1', 'tower_2', 'tower_3')
-TowerOneToTowerThree(user_input, 'tower_1', 'tower_2', 'tower_3') 
-TowerTwoToTowerOne(user_input, 'tower_2', 'tower_3', 'tower_1')
-TowerTwoToTowerThree(user_input, 'tower_2', 'tower_1', 'tower_3')
-TowerThreeToTowerOne(user_input, 'tower_3', 'tower_2', 'tower_1')
-TowerThreeToTowerTwo(user_input, 'tower_3', 'tower_1', 'tower_2')
