78a8a768080b73cbf64ab79f43cb2fecf5624f79
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-27 14:03:01 +0800

    Added pseudocode for binary tree, will make it more specific later

---
+++ core_programs/3_binary_tree.py
@@ -0,0 +1,8 @@
+#Binary Tree Pseudocode
+#Make user input numbers
+#Insert the input into tree (left to right per leve)
+#Let user input until maximum levels are reached (max of 5 levels, 32 inputs)
+    #Ask user if they want to add another level after filling in each level. User can:
+        # Add level
+        # finish input
+#Print Tree(?) print the traversal for tree (preorder, inorder, postorder) when any of the two ocnditions are met
