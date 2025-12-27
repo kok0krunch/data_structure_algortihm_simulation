ce470e27da5e5e18a9533dda3b7697d396b0636d
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-12-27 14:06:58 +0800

    Added code taken from the code of binary search tree since both are binary trees and contain similar attributes.

--- core_programs/3_binary_tree.py
+++ core_programs/3_binary_tree.py
@@ -1,8 +1,22 @@
 #Binary Tree Pseudocode
+class Node:
+    def __init__(self,number): # Class attributes: top, left, right
+        self.number=number
+        self.left=None
+        self.right=None
+
+class BinaryTree: 
+    def __init__(self):
+        self.root=None
+
+    # Functions needed
+    def insert(self, number): #Insert the input into tree (left to right per level)
+        if self.root==None: # First number as root
+            self.root=Node(number)
+
 #Make user input numbers
-#Insert the input into tree (left to right per leve)
 #Let user input until maximum levels are reached (max of 5 levels, 32 inputs)
     #Ask user if they want to add another level after filling in each level. User can:
         # Add level
         # finish input
-#Print Tree(?) print the traversal for tree (preorder, inorder, postorder) when any of the two ocnditions are met
+#Print Tree(optional), print the traversal for tree (preorder, inorder, postorder) when any of the two conditions are met
