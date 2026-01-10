e2c2c3f6fc05a82ac7eeb54052030f22e5a7c4e6
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2026-01-11 01:14:00 +0800

    i have done the general GUI for this code, however, i did it in a separate file so that i can play around. now im trasferring thing to the main py file. i first added the variables needed

--- pygame files/recursion_pygame.py
+++ pygame files/recursion_pygame.py
@@ -1,64 +1,20 @@
 import pygame
+import sys
+import time 
 
 pygame.init()
 
 # Images
-background_img = pygame.image.load
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Tower of Hanoi')
 clock = pygame.time.Clock()
 
-class TowerOFHanoi:
-    def __init__(self, tower_1, tower_2, tower_3):
-        self.tower_1 = 1
-        self.tower_2 = 2
-        self.tower_3 = 3
-
-
-
-    # Functions for solving Tower of Hanoi using recursion
-    def tower_one_to_tower_two(self, user_input, tower_1, tower_2, tower_3):
-        if user_input == 0:
-            return
-        self.tower_one_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
-        print("Move disk", user_input, "from", tower_1, "to", tower_2)
-        self.tower_one_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
-
-    def tower_one_to_tower_three(self, user_input, tower_1, tower_2, tower_3):
-        if user_input == 0:
-            return
-        
-        self.tower_one_to_tower_three(int(user_input) - 1, tower_1, tower_3, tower_2)
-        print('Move disk', user_input, 'from', tower_1, 'to', tower_3)
-        self.tower_one_to_tower_three(int(user_input) - 1, tower_2, tower_1, tower_3)
-
-    def tower_two_to_tower_one(self, user_input, tower_2, tower_3, tower_1):
-        if user_input == 0:
-            return
-        
-        self.tower_two_to_tower_one(int(user_input) - 1, tower_2, tower_1, tower_3)
-        print("Move disk", user_input, "from", tower_2, "to", tower_1)
-        self.tower_two_to_tower_one(int(user_input) - 1, tower_3, tower_2, tower_1)
-
-    def tower_two_to_tower_three(self, user_input, tower_2, tower_1, tower_3):
-        if user_input == 0:
-            return
-        self.tower_two_to_tower_three(int(user_input) - 1, tower_2, tower_3, tower_1)
-        print("Move disk", user_input, "from", tower_2, "to", tower_3)
-        self.tower_two_to_tower_three(int(user_input) - 1, tower_1, tower_2, tower_3)
-
-    def tower_three_to_tower_one(self, user_input, tower_3, tower_2, tower_1):
-        if user_input == 0:
-            return
-        self.tower_three_to_tower_one(int(user_input) - 1, tower_3, tower_1, tower_2)
-        print("Move disk", user_input, "from", tower_3, "to", tower_1)
-        self.tower_three_to_tower_one(int(user_input) - 1, tower_2, tower_3, tower_1)
-
-    def tower_three_to_tower_two(self, user_input, tower_3, tower_1, tower_2):
-        if user_input == 0:
-            return
-        self.tower_three_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
-        print("Move disk", user_input, "from", tower_3, "to", tower_2)
-        self.tower_three_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
-
-    def
+max_disks = 7
+peg_y_placement = 120
+peg_x_placement = { 1: 250, 2: 450, 3: 650}
+black = (0, 0, 0)
+blue = (100, 149, 237)
+white = (250, 250, 250)
+move_delay = 0.6
+peg_img = pygame.image.load("images/peg_img.png").convert_alpha()
+disk_img = pygame.image.load("images/disk_img.png").convert_alpha()
