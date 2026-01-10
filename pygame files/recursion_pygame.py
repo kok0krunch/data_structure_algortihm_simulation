04a8cf6373c259db3bbcaaa0e42f33746901cef8
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2026-01-11 01:15:52 +0800

    feat: added the actual logic for tower of hanoi with a bit of modification to fit the animation in pygame

--- pygame files/recursion_pygame.py
+++ pygame files/recursion_pygame.py
@@ -4,11 +4,9 @@ import time
 
 pygame.init()
 
-# Images
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Tower of Hanoi')
 clock = pygame.time.Clock()
-
 max_disks = 7
 peg_y_placement = 120
 peg_x_placement = { 1: 250, 2: 450, 3: 650}
@@ -18,3 +16,56 @@ white = (250, 250, 250)
 move_delay = 0.6
 peg_img = pygame.image.load("images/peg_img.png").convert_alpha()
 disk_img = pygame.image.load("images/disk_img.png").convert_alpha()
+
+class TowerOfHanoi:
+    def __init__(self):
+        self.tower_1 = 1
+        self.tower_2 = 2
+        self.tower_3 = 3
+        self.moves = []
+
+
+    # Functions for solving Tower of Hanoi using recursion
+    def tower_one_to_tower_two(self, user_input, tower_1, tower_2, tower_3):
+        if user_input == 0:
+            return
+        self.tower_one_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
+        self.moves.append((tower_1, tower_2))
+        self.tower_one_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
+
+    def tower_one_to_tower_three(self, user_input, tower_1, tower_2, tower_3):
+        if user_input == 0:
+            return
+        
+        self.tower_one_to_tower_three(int(user_input) - 1, tower_1, tower_3, tower_2)
+        self.moves.append((tower_1, tower_3))
+        self.tower_one_to_tower_three(int(user_input) - 1, tower_2, tower_1, tower_3)
+
+    def tower_two_to_tower_one(self, user_input, tower_2, tower_3, tower_1):
+        if user_input == 0:
+            return
+        
+        self.tower_two_to_tower_one(int(user_input) - 1, tower_2, tower_1, tower_3)
+        self.moves.append((tower_2, tower_1))
+        self.tower_two_to_tower_one(int(user_input) - 1, tower_3, tower_2, tower_1)
+
+    def tower_two_to_tower_three(self, user_input, tower_2, tower_1, tower_3):
+        if user_input == 0:
+            return
+        self.tower_two_to_tower_three(int(user_input) - 1, tower_2, tower_3, tower_1)
+        self.moves.append((tower_2, tower_3))
+        self.tower_two_to_tower_three(int(user_input) - 1, tower_1, tower_2, tower_3)
+
+    def tower_three_to_tower_one(self, user_input, tower_3, tower_2, tower_1):
+        if user_input == 0:
+            return
+        self.tower_three_to_tower_one(int(user_input) - 1, tower_3, tower_1, tower_2)
+        self.moves.append((tower_3, tower_1))
+        self.tower_three_to_tower_one(int(user_input) - 1, tower_2, tower_3, tower_1)
+
+    def tower_three_to_tower_two(self, user_input, tower_3, tower_1, tower_2):
+        if user_input == 0:
+            return
+        self.tower_three_to_tower_two(int(user_input) - 1, tower_3, tower_2, tower_1)
+        self.moves.append((tower_3, tower_2))
+        self.tower_three_to_tower_two(int(user_input) - 1, tower_1, tower_3, tower_2)
