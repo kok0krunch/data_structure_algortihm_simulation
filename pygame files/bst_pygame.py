5a78e6d7fde99c84b06b989f6cd65bd2f0baf87f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-01-13 18:32:23 +0800

    Added: Since the code for left and irght node are similar I copy pasted the left node to create the irght node, but instead of subtracting the x-intercept, I added it. Still working on presenting the right numbers though.

--- pygame files/bst_pygame.py
+++ pygame files/bst_pygame.py
@@ -10,14 +10,20 @@ from binary_search_tree import BinarySearchTree
 def draw_bst(screen, node, x, y, font):
     if node is None:
         return
-    pygame.draw.circle(screen, (0, 120, 255), (x, y), 100) # draw circle for node
+    pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
     text = font.render(str(node.number), True, (255, 255, 255)) #determines characteristic of text
     screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
 
     if node.left: #left child, number is lower/ equal to root.
-        pygame.draw.circle(screen, (0, 120, 255), (x, y), 100) # draw circle for node
+        pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
         pygame.draw.line(screen, (0, 0, 0), (x, y), (x - 80, y + 70), 2)
         draw_bst(screen, node.left, x - 80, y + 70, font)
+        screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
+    
+    if node.right: #right child, number is higher than the root.
+        pygame.draw.circle(screen, (0, 120, 255), (x, y), 500) # draw circle for node
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (x - 80, y + 70), 2)
+        draw_bst(screen, node.left, x + 80, y + 70, font)
         screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
 
 def bst_menu(screen, clock, globalbg_img, back_btn):
