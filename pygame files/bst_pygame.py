8b1776fbf692a2d40dbe4c5224d873887146002f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-01-13 18:44:03 +0800

    Test: Experimenting the format of the tree, to ensure that every input will not overlap.

--- pygame files/bst_pygame.py
+++ pygame files/bst_pygame.py
@@ -16,13 +16,13 @@ def draw_bst(screen, node, x, y, font):
 
     if node.left: #left child, number is lower/ equal to root.
         pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
-        pygame.draw.line(screen, (0, 0, 0), (x, y), (x - 80, y + 70), 2)
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (x - 100, y + 100), 2)
         draw_bst(screen, node.left, x - 80, y + 70, font)
         screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
     
     if node.right: #right child, number is higher than the root.
         pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
-        pygame.draw.line(screen, (0, 0, 0), (x, y), (x + 80, y + 70), 2)
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (x + 100, y + 100), 2)
         draw_bst(screen, node.right, x + 80, y + 70, font)
         screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
 
