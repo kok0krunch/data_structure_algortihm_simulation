3179da2253b6907b4b9fc17630f621f061638929
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-01-13 21:41:18 +0800

    Added: bst works now, but some nodes overlap. I added some horizontal(h_spacing) and vertical (v_spacing), so that nodes won't overlap. Still not running properly, but I'm working on it.

--- pygame files/bst_pygame.py
+++ pygame files/bst_pygame.py
@@ -7,7 +7,7 @@ bst_path = os.path.abspath(os.path.join(current_dir, "..", "core_programs", "cor
 sys.path.append(bst_path)
 from binary_search_tree import BinarySearchTree
 
-def draw_bst(screen, node, x, y, level, font):
+def draw_bst(screen, node, x, y, font, h_spacing=120, v_spacing=300):
     if node is None:
         return
     pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
@@ -15,16 +15,20 @@ def draw_bst(screen, node, x, y, level, font):
     screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
 
     if node.left: #left child, number is lower/ equal to root.
-        pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
-        pygame.draw.line(screen, (0, 0, 0), (x, y), (x - 100, y + 100), 2)
-        draw_bst(screen, node.left, x - 80, y + 70, level+1, font)
-        screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
+        child_x = x - h_spacing
+        child_y = y + v_spacing
+        pygame.draw.circle(screen, (0, 120, 255), (child_x, child_y), 50) # draw circle for node
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (child_x, child_y), 2)
+        draw_bst(screen, node.left, child_x, child_y, font, h_spacing / 1.5, v_spacing)
+        screen.blit(text, text.get_rect(center=(child_x, child_y))) #shows text in screen
     
     if node.right: #right child, number is higher than the root.
-        pygame.draw.circle(screen, (0, 120, 255), (x, y), 50) # draw circle for node
-        pygame.draw.line(screen, (0, 0, 0), (x, y), (x + 100, y + 100), 2)
-        draw_bst(screen, node.right, x + 80, y + 70, level+1, font)
-        screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
+        child_x = x + h_spacing
+        child_y = y + v_spacing
+        pygame.draw.circle(screen, (0, 120, 255), (child_x, child_y), 50) # draw circle for node
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (child_x, child_y), 2)
+        draw_bst(screen, node.right, child_x, child_y, font, h_spacing / 1.5, v_spacing)
+        screen.blit(text, text.get_rect(center=(child_x, child_y))) #shows text in screen
 
 def bst_menu(screen, clock, globalbg_img, back_btn):
     """Binary Search Tree menu function"""
@@ -58,7 +62,7 @@ def bst_menu(screen, clock, globalbg_img, back_btn):
                     user_input += event.unicode
         
         if bst.root:
-            draw_bst(screen, bst.root, screen.get_width()//2, 120, 0, font)
+            draw_bst(screen, bst.root, screen.get_width()//2, 120, font)
 
         # Draw back button
         if back_btn.draw():
