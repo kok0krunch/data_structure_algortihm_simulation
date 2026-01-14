c944fd1f4591e1d62c3dc8fd87cf375c9fb1cd0c
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2026-01-14 13:07:14 +0800

    Added: Added design to nodes so it looks appealing.

--- pygame files/bst_pygame.py
+++ pygame files/bst_pygame.py
@@ -8,9 +8,12 @@ sys.path.append(bst_path)
 from binary_search_tree import BinarySearchTree
 
 def draw_bst(screen, node, x, y, font, h_spacing=500, v_spacing=100):
+    node_img = pygame.image.load("images/bst_node.png").convert_alpha()
+    node_img = pygame.transform.scale(node_img, (150, 150))
     if node is None:
         return
-    pygame.draw.circle(screen, (0, 120, 255), (x, y), 30) # draw circle for node
+    node_rect = node_img.get_rect(center=(x, y+15))
+    screen.blit(node_img, node_rect)
     text = font.render(str(node.number), True, (255, 255, 255)) #determines characteristic of text
     screen.blit(text, text.get_rect(center=(x, y))) #shows text in screen
 
@@ -19,7 +22,8 @@ def draw_bst(screen, node, x, y, font, h_spacing=500, v_spacing=100):
         child_y = y + v_spacing
         new_h_spacing=max(h_spacing/2,30)
         pygame.draw.line(screen, (0, 0, 0), (x, y), (child_x, child_y), 2)
-        pygame.draw.circle(screen, (0, 120, 255), (child_x, child_y), 30) # draw circle for node
+        node_rect = node_img.get_rect(center=(child_x, child_y))
+        screen.blit(node_img, node_rect)
         draw_bst(screen, node.left, child_x, child_y, font, new_h_spacing, v_spacing)
     
     if node.right: #right child, number is higher than the root.
@@ -27,7 +31,8 @@ def draw_bst(screen, node, x, y, font, h_spacing=500, v_spacing=100):
         child_y = y + v_spacing
         new_h_spacing=max(h_spacing/2,30)
         pygame.draw.line(screen, (0, 0, 0), (x, y), (child_x, child_y), 2)
-        pygame.draw.circle(screen, (0, 120, 255), (child_x, child_y), 30) # draw circle for node
+        node_rect = node_img.get_rect(center=(child_x, child_y))
+        screen.blit(node_img, node_rect)
         draw_bst(screen, node.right, child_x, child_y, font, new_h_spacing, v_spacing)
 
 def bst_menu(screen, clock, globalbg_img, back_btn):
@@ -82,12 +87,12 @@ def bst_menu(screen, clock, globalbg_img, back_btn):
         
         if bst.root:
             draw_bst(screen, bst.root, screen.get_width()//2+tree_offset_x, 180+tree_offset_y, font)
-            
+
         instruction_font = pygame.font.SysFont(None, 48)
         instruction = "Welcome to Binary Search Tree maker! Input your number and enter to\n see it in front of you. If your binary search tree is cut off screen, use your \n up, left, down, and right keys to maneuver the tree. "
         instruction_surface = instruction_font.render(instruction, True, (0,0,0))
         x_margin=180
-        y_margin=20
+        y_margin=20+tree_offset_y
         instruction_rect = instruction_surface.get_rect(topleft=(x_margin,y_margin))
         screen.blit(instruction_surface, instruction_rect)
 
