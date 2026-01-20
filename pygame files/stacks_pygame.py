f263eb153841953463adbcacebf5aeaeed5ce60d
kok0krunch <amaliakadoi@gmail.com>
2026-01-20 18:02:44 +0800

    Added: Copied the class stack from 1_stacks.py

--- pygame files/stacks_pygame.py
+++ pygame files/stacks_pygame.py
@@ -1,5 +1,28 @@
 import pygame
 
+class Stack:
+    def __init__(self):
+        self.items = [] # List to hold stack items
+        
+    def push(self, item):
+        self.items.append(item) # Add item to the list 
+        
+    def pop(self): # Remove item from the top of the stack
+        if not self.is_empty():
+            return self.items.pop() 
+        return None
+        
+    def is_empty(self): # Check if list is empty
+        return len(self.items) == 0 
+        
+    def size(self): # Number of items in the stack
+        return len(self.items) 
+        
+    def peek(self): # Return the top item without removing it
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+    
 def stacks_menu(screen, clock, globalbg_img, back_btn):
     """Stacks menu function"""
     running = True
